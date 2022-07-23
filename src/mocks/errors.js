116c9d1a358500c8fb646e329fb5feaec6bcdf87
mavuriel <urieljtenorio@gmail.com>
2022-07-23 17:34:10 -0500

    Add errors mocks

---
+++ src/mocks/errors.js
@@ -0,0 +1,12 @@
+const expired = {
+  errors: [
+    {
+      code: 38192,
+      title: 'Access token expired',
+      detail: 'The access token is expired',
+      status: 401,
+    },
+  ],
+}
+
+export { expired }
