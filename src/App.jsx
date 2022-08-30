de4f00492448b1e690ddc54855139c3bef689713
mavuriel <urieljtenorio@gmail.com>
2022-08-29 23:40:44 -0500

    Modify app component for route render

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,38 @@
-// import { useEffect, useState } from 'react'
-
-// import FlightMockOption from './components/FlightMockOption'
-// import SingleCalendar from './components/SingleCalendar'
-// import DualCalendar from './components/DualCalendar'
-// import useDataClient from './hooks/useDataClient'
-
-import { ErrorMessage, Field, Form, Formik } from 'formik'
-import { useState } from 'react'
-import { Outlet, Route, Routes, useNavigate } from 'react-router-dom'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import FlightList from './components/FlightList'
 
 function App() {
-  const [formSend, setFormSend] = useState(false)
-
-  const navigate = useNavigate()
-
   return (
     <>
+      <Header />
+
       <Routes>
         <Route
           path='/'
-          element={<div>hola</div>}
+          element={<Home />}
         />
         <Route
-          path='test'
-          element={<div>prueba</div>}
+          path='flights/:origen/to/:destino/:ida'
+          element={<FlightList />}
+        >
+          <Route
+            path=':vuelta'
+            element={<FlightList />}
+          />
+        </Route>
+
+        <Route
+          path='*'
+          element={<NotFound />}
         />
       </Routes>
-
-      <h1>Buscador de vuelos</h1>
-
-      <Formik
-        initialValues={{
-          origen: '',
-          destino: '',
-        }}
-        validate={({ origen, destino }) => {
-          const errors = {}
-
-          if (!origen) errors.origen = 'Ingresa un origen'
-          if (!destino) errors.destino = 'Ingresa un destino'
-
-          return errors
-        }}
-        onSubmit={(valores, { resetForm }) => {
-          // TODO: necesito limpiar el form??
-          resetForm()
-          console.log('formulario enviado')
-          console.table(valores)
-          // TODO: hacer la consulta a la api
-
-          setFormSend(true)
-
-          navigate('/test')
-
-          setTimeout(() => setFormSend(false), 3000)
-        }}
-      >
-        {({ errors, isSubmitting }) => (
-          <Form action='/test'>
-            <div>
-              <label htmlFor='origen'>Origen</label>
-              <Field
-                type='text'
-                id='origen'
-                name='origen'
-                placeholder='Mexico'
-              />
-              <ErrorMessage
-                name='origen'
-                component={() => <div>{errors.origen}</div>}
-              />
-            </div>
-            <div>
-              <label htmlFor='destino'>Destino</label>
-              <Field
-                type='text'
-                id='destino'
-                name='destino'
-                placeholder='Mexico'
-              />
-              <ErrorMessage
-                name='destino'
-                component={() => <div>{errors.destino}</div>}
-              />
-            </div>
-            <button
-              type='submit'
-              disabled={isSubmitting}
-            >
-              Enviar
-            </button>
-            {formSend && <p>Realizando la consulta</p>}
-          </Form>
-        )}
-      </Formik>
 
       <Outlet />
     </>
   )
-  // return <FlightMockOption />
-  /* return (
-    <>
-      <DualCalendar />
-      <br />
-      <hr />
-      <br />
-      <SingleCalendar />
-    </>
-  ) */
-  // const { countryClient, currencyClient } = useDataClient()
-  // console.log({ countryClient, currencyClient })
-  // const { country, countryCode } = countryClient
 }
 
 // TODO: guardar en local la data del cliente
