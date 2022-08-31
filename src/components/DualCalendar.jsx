5bc080001f162ab84c4ab4cd33891853ec25b1a2
mavuriel <urieljtenorio@gmail.com>
2022-08-31 00:33:19 -0500

    Modify dual calendar component to use with formik

--- src/components/DualCalendar.jsx
+++ src/components/DualCalendar.jsx
@@ -1,34 +1,50 @@
+import PropTypes from 'prop-types'
 import { useCallback, useEffect, useState } from 'react'
 import { Calendar } from '@natscale/react-calendar'
 
 // TODO: cambiar luego con tailwind
 import '@natscale/react-calendar/dist/main.css'
 import useFormat from '../hooks/useFormat'
+export default function DualCalendar({ setFieldValue }) {
+  const [date, setDate] = useState([])
 
-export default function DualCalendar() {
-  const [value, setValue] = useState([])
+  const [datesFormat, setDatesFormat] = useState([])
 
   useEffect(() => {
-    if (!value.length) return
-    console.log(value)
-    console.log(useFormat({ dates: value, type: 'api' }))
-    console.log(useFormat({ dates: value, type: 'user' }))
-  }, [value])
+    if (!date.length) return
+    const api = useFormat({ dates: date, type: 'api' })
+    const user = useFormat({ dates: date, type: 'user' })
+
+    setDatesFormat({ api, user })
+  }, [date])
 
   const onChange = useCallback(
     val => {
-      setValue(val)
+      setDate(val)
     },
-    [setValue]
+    [setDate]
   )
+
+  useEffect(() => {
+    // ? investigar si hay una mejor manera de hacer esto
+    const setFieldDate = async () => {
+      setFieldValue('fechas', datesFormat)
+    }
+
+    setTimeout(() => setFieldDate(), 1000)
+  }, [datesFormat])
 
   return (
     <Calendar
       showDualCalendar
       isRangeSelector
       useDarkMode
-      value={value}
+      value={date}
       onChange={onChange}
     />
   )
+}
+
+DualCalendar.propTypes = {
+  setFieldValue: PropTypes.func,
 }
